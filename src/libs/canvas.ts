ed62f1dfe5f8ef5608a6c8fc1c205fcb14a6cec0
Masaya Kazama <miyaokamiyao@gmail.com>
2024-10-06 06:55:30 +0000

    WIP

--- src/libs/canvas.ts
+++ src/libs/canvas.ts
@@ -1,54 +1,39 @@
-interface CalculateVideoRectsParams {
-  containerWidth: number;
-  containerHeight: number;
-  aspectRatio: number;
-  count: number;
-}
-
 // 最適な縦横セル数を計算
 export function calculateOptimalCellCount(
   containerWidth: number,
   containerHeight: number,
-  aspectRatio: number,
-  count: number,
-): { rows: number; cols: number } {
-  const rows = Math.ceil(Math.sqrt((count * containerWidth) / containerHeight / aspectRatio));
-  const cols = Math.ceil(count / rows);
-  return { rows, cols };
+  contentAspectRatio: number,
+  contentCount: number,
+): GridCellCount {
+  const hCellCount = Math.ceil(
+    Math.sqrt((contentCount * containerWidth) / containerHeight / contentAspectRatio),
+  );
+  const vCellCount = Math.ceil(contentCount / hCellCount);
+  return { hCellCount, vCellCount };
 }
 
-// 指定の縦横セル数で分割したセルに配置できる指定アスペクト比の矩形面積を計算
-export function calculateArea({
-  containerWidth,
-  containerHeight,
-  aspectRatio,
-  rows,
-  cols,
-}: {
-  containerWidth: number;
-  containerHeight: number;
-  aspectRatio: number;
-  rows: number;
-  cols: number;
-}): { width: number; height: number } {
-  const cellWidth = containerWidth / rows;
-  const cellHeight = containerHeight / cols;
+interface GridCellCount {
+  hCellCount: number;
+  vCellCount: number;
+}
 
-  const areaInCell = calculateAreaInCell(cellWidth, cellHeight, aspectRatio);
+export interface GridCell {
+  span: number;
+  isHorizontal: boolean;
+}
 
-  return areaInCell;
+export interface GridLayout {
+  contentAreaTotal: number;
+  cellList: GridCell[];
+  gridStyle: Record<string, string>;
 }
 
 // 指定したカウントで必要な縦横セル数の組み合わせを列挙
-export function enumerateCellCounts({
-  count,
-}: {
-  count: number;
-}): { horizontal: number; vertical: number }[] {
-  const results: { horizontal: number; vertical: number }[] = [];
+export function enumerateCellCounts({ count }: { count: number }): GridCellCount[] {
+  const results: GridCellCount[] = [];
   for (let horizontal = 1; horizontal <= count; horizontal++) {
     const vertical = Math.ceil(count / horizontal);
-    results.push({ horizontal, vertical });
+    results.push({ hCellCount: horizontal, vCellCount: vertical });
   }
   return results;
 }
@@ -75,4 +60,89 @@ export function calculateAreaInCell(
     width: cellWidth,
     height: cellWidth / aspectRatio,
   };
+}
+
+export function calculateGridLayout({
+  canvasWidth,
+  canvasHeight,
+  cellCount,
+  contentCount,
+  contentAspectRatio,
+}: {
+  canvasWidth: number;
+  canvasHeight: number;
+  cellCount: GridCellCount;
+  contentCount: number;
+  contentAspectRatio: number;
+}): GridLayout {
+  const { hCellCount, vCellCount } = cellCount;
+
+  // const title = `${hCellCount}x${vCellCount}`;
+
+  const totalCellCount = hCellCount * vCellCount;
+  // 余るcellの数
+  const remainingCells = totalCellCount - contentCount;
+
+  // 1列目はは余るcellの分減らしてカラム幅を広げる
+  const firstRowColumns = hCellCount - remainingCells;
+  // 2列目以降はそのまま
+  const otherRowColumns = hCellCount;
+
+  // gridのspan数を計算
+  // 1列目と2列目のカラム数の最小公倍数を使って基本単位を決定
+  const spanCount = getLCM(firstRowColumns, otherRowColumns);
+
+  // グリッドテンプレート列を設定
+  const gridStyle = {
+    gridTemplateColumns: `repeat(${spanCount}, 1fr)`, // horizontal列
+    gridTemplateRows: Array.from({ length: vCellCount }, () => "1fr").join(" "), // vertical行
+  };
+
+  // span幅
+  const spanWidth = canvasWidth / spanCount;
+  // cellの高さ
+  const cellHeight = canvasHeight / vCellCount;
+
+  let contentAreaTotal = 0;
+  const cellList: GridCell[] = [];
+  for (let v = 0; v < vCellCount; v++) {
+    for (let h = 0; h < hCellCount; h++) {
+      const cellIndex = v * hCellCount + h;
+      // cellに割り当てるspan数
+      const span = spanCount / (cellIndex < firstRowColumns ? firstRowColumns : otherRowColumns);
+
+      // セル幅とアスペクト比を算出
+      const cellWidth = spanWidth * span;
+      const cellAspectRatio = cellWidth / cellHeight;
+
+      // cellがcontentより横長か
+      const isHorizontal = cellAspectRatio > contentAspectRatio;
+
+      const contentWidth = isHorizontal ? cellHeight * contentAspectRatio : cellWidth;
+      const contentHeight = isHorizontal ? cellHeight : cellWidth / contentAspectRatio;
+
+      cellList.push({
+        span,
+        isHorizontal,
+      });
+
+      contentAreaTotal += contentWidth * contentHeight;
+    }
+  }
+
+  return {
+    cellList,
+    contentAreaTotal,
+    gridStyle,
+  };
+}
+
+// 最大公約数を求める関数
+function getGCD(a: number, b: number): number {
+  return b === 0 ? a : getGCD(b, a % b);
+}
+
+// 最小公倍数を求める関数
+function getLCM(a: number, b: number): number {
+  return (a * b) / getGCD(a, b);
 }
--- src/views/LayoutDemoView.vue
+++ src/views/LayoutDemoView.vue
@@ -1,100 +1,27 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
-import { enumerateCellCounts } from "@/libs/canvas";
+import { calculateGridLayout, enumerateCellCounts, type GridLayout } from "@/libs/canvas";
 
 const canvasWidth = ref(400);
 const canvasHeight = ref(300);
-const rectCount = ref(5);
-const rectAspectRatio = ref(16 / 9);
+const contentCount = ref(5);
+const contentAspectRatio = ref(16 / 9);
 
-const cellCountList = computed(() => enumerateCellCounts({ count: rectCount.value }));
+const cellCountList = computed(() => enumerateCellCounts({ count: contentCount.value }));
 
-interface CellLayout {
-  title: string;
-  isHorizontal: boolean;
-  rectArea: number;
-  cellCount: {
-    horizontal: number;
-    vertical: number;
-  };
-  spanList: number[];
-  columnTemplate: string;
-}
-const cellRectList = computed<CellLayout[]>(() => {
+const gridLayoutList = computed<GridLayout[]>(() => {
   const result = cellCountList.value.map((cellCount) => {
-    const { horizontal, vertical } = cellCount;
-
-    const totalCellCount = horizontal * vertical;
-    // 余ったcellの数
-    const remainingCells = totalCellCount - rectCount.value;
-
-    const firstRowColumns = horizontal - remainingCells;
-    const otherRowColumns = horizontal;
-    // 1つの基本単位の幅を計算
-    // 最小公倍数を使って基本単位を決定
-    const lcm = getLCM(firstRowColumns, otherRowColumns);
-
-    // グリッドテンプレート列を設定
-    const columnTemplate = `repeat(${lcm}, 1fr)`;
-
-    const spanList = [];
-    const cellWidth = canvasWidth.value / horizontal;
-    const cellHeight = canvasHeight.value / vertical;
-    const cellAspectRatio = cellWidth / cellHeight;
-
-    // rectのaspectRatioよりcellのaspectRatioが大きい場合はcellHeightを使う
-
-    // cellがrectより横長か
-    const isHorizontal = cellAspectRatio > rectAspectRatio.value;
-
-    const rectWidth = isHorizontal ? cellHeight * rectAspectRatio.value : cellWidth;
-    const rectHeight = isHorizontal ? cellHeight : cellWidth / rectAspectRatio.value;
-    const rectArea = rectWidth * rectHeight;
-    const title = `${horizontal}x${vertical}`;
-
-    const grow = horizontal - remainingCells;
-    for (let v = 0; v < vertical; v++) {
-      for (let h = 0; h < horizontal; h++) {
-        const cellIndex = v * horizontal + h;
-        spanList.push(lcm / (cellIndex < grow ? firstRowColumns : otherRowColumns));
-      }
-    }
-    return {
-      title,
-      isHorizontal,
-      rectArea,
-      spanList,
+    return calculateGridLayout({
+      canvasWidth: canvasWidth.value,
+      canvasHeight: canvasHeight.value,
       cellCount,
-      columnTemplate,
-      remainingCells,
-    };
+      contentCount: contentCount.value,
+      contentAspectRatio: contentAspectRatio.value,
+    });
   });
 
-  return result.sort((a, b) => b.rectArea - a.rectArea);
+  return result.sort((a, b) => b.contentAreaTotal - a.contentAreaTotal);
 });
-
-function getStyle(cellLayout: CellLayout) {
-  const { horizontal, vertical } = cellLayout.cellCount;
-
-  return {
-    display: "grid",
-    width: `${canvasWidth.value}px`,
-    height: `${canvasHeight.value}px`,
-
-    gridTemplateColumns: cellLayout.columnTemplate, // horizontal列
-    gridTemplateRows: Array.from({ length: vertical }, () => "1fr").join(" "), // vertical行
-  };
-}
-
-// 最大公約数を求める関数
-function getGCD(a: number, b: number): number {
-  return b === 0 ? a : getGCD(b, a % b);
-}
-
-// 最小公倍数を求める関数
-function getLCM(a: number, b: number): number {
-  return (a * b) / getGCD(a, b);
-}
 </script>
 
 <template>
@@ -102,19 +29,19 @@ function getLCM(a: number, b: number): number {
     <div class="fixed left-2 top-2 z-10 flex flex-col bg-gray-100 p-2 shadow">
       <label>
         <p>Canvas Width: {{ canvasWidth }}</p>
-        <input type="range" v-model="canvasWidth" min="50" max="500" step="1" />
+        <input type="range" v-model="canvasWidth" min="50" max="1000" step="1" />
       </label>
       <label>
         <p>Canvas Height: {{ canvasHeight }}</p>
-        <input type="range" v-model="canvasHeight" min="50" max="500" step="1" />
+        <input type="range" v-model="canvasHeight" min="50" max="1000" step="1" />
       </label>
       <label>
-        <p>Rect Count: {{ rectCount }}</p>
-        <input type="range" v-model="rectCount" min="1" max="16" step="1" />
+        <p>Content Count: {{ contentCount }}</p>
+        <input type="range" v-model="contentCount" min="1" max="20" step="1" />
       </label>
       <label>
-        <p>Rect Aspect Ratio: {{ Number(rectAspectRatio).toFixed(2) }}</p>
-        <input type="range" v-model="rectAspectRatio" min="1" max="4" step="0.01" />
+        <p>Content Aspect Ratio: {{ Number(contentAspectRatio).toFixed(2) }}</p>
+        <input type="range" v-model="contentAspectRatio" min="1" max="4" step="0.01" />
       </label>
       <div>
         <p>Cell Count List</p>
@@ -126,40 +53,46 @@ function getLCM(a: number, b: number): number {
 
     <div class="grid gap-10">
       <div
-        v-for="(cellRectPattern, i) in cellRectList"
-        :key="i"
-        class="relative outline"
-        :style="getStyle(cellRectPattern)"
+        class="relative"
+        v-for="(gridLayout, gridLayoutIdx) in gridLayoutList"
+        :key="gridLayoutIdx"
       >
         <div class="absolute right-full flex flex-col px-2">
-          <p>{{ cellRectPattern.title }}</p>
           <p>
-            {{ cellRectPattern.isHorizontal ? "Horizontal" : "Vertical" }}
-          </p>
-          <p>
-            {{ cellRectPattern.rectArea.toFixed(1) }}
-          </p>
-          <p>
-            remainingCells:
-            {{ cellRectPattern.remainingCells }}
+            contentAreaTotal:
+            {{ gridLayout.contentAreaTotal.toFixed(1) }}
           </p>
         </div>
+
         <div
-          v-for="(span, j) in cellRectPattern.spanList"
-          :key="j"
-          class="flex items-center justify-center overflow-hidden outline outline-1 outline-gray-300"
+          class="grid outline"
           :style="{
-            gridColumn: `span ${span}`,
+            ...gridLayout.gridStyle,
+            width: `${canvasWidth}px`,
+            height: `${canvasHeight}px`,
           }"
         >
           <div
-            v-if="j < rectCount"
-            :class="`bg-gray-600 text-white text-sm flex items-center justify-center ${cellRectPattern.isHorizontal ? 'h-full' : 'w-full'}`"
+            v-for="(cell, cellIdx) in gridLayout.cellList"
+            :key="cellIdx"
+            class="flex items-center justify-center overflow-hidden outline outline-1 outline-gray-300"
             :style="{
-              aspectRatio: `${rectAspectRatio}`,
+              gridColumn: `span ${cell.span}`,
             }"
           >
-            {{ j + 1 }}
+            <div
+              class="flex flex-col"
+              v-if="cellIdx < contentCount"
+              :class="`bg-gray-600 text-white text-sm flex items-center justify-center ${cell.isHorizontal ? 'h-full' : 'w-full'}`"
+              :style="{
+                aspectRatio: `${contentAspectRatio}`,
+              }"
+            >
+              <p>#{{ cellIdx }}</p>
+              <p>
+                {{ cell.span }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
