5182ef59fb84cd3445ae5ab1c670d34522c6852c
Masaya Kazama <miyaokamiyao@gmail.com>
2024-10-13 01:42:01 +0000

    チャット欄の高さを半分にし、左右配置を切り替えられるようにした

--- src/components/YoutubeLiveChat.vue
+++ src/components/YoutubeLiveChat.vue
@@ -1,9 +1,51 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 
 const props = defineProps<{
   videoId: string;
 }>();
+
+// ヘッダーの高さ
+const headerHeight = 50;
+// スパチャの高さ
+const tickerHeight = 40;
+// 固定メッセージの高さ
+const pinnedHeight = 56;
+// フッターの高さ
+const footerHeight = 52 + 2;
+// メッセージの左右padding
+const msgPaddingWidth = 24;
+// メッセージのauthorアイコン+margin幅
+const authorWidth = 40;
+
+const containerPadding = 8;
+const containerHeight = "40%";
+
+const clipTop = headerHeight + tickerHeight + pinnedHeight;
+const clipBottom = footerHeight;
+const clipRight = msgPaddingWidth - containerPadding;
+const clipLeft = authorWidth + msgPaddingWidth - containerPadding;
+const clipInset = [clipTop, clipRight, clipBottom, clipLeft].map((v) => `${v}px`).join(" ");
+const clipWidth = 300 - clipRight - clipLeft;
+
+const clipStyle = {
+  top: `-${clipTop}px`,
+  right: `-${clipRight}px`,
+  left: `-${clipLeft}px`,
+  clipPath: `inset(${clipInset})`,
+  height: `calc(100% + ${clipTop + clipBottom}px)`,
+};
+
+// 左右どちらに配置するか
+const isRight = ref(true);
+
+const containerStyle = computed(() => {
+  return {
+    height: containerHeight,
+    width: `${clipWidth}px`,
+    ...(isRight.value ? { right: 0 } : { left: 0 }),
+  };
+});
 
 const chatUrl = computed(() => {
   const url = new URL("https://www.youtube.com/live_chat");
@@ -12,12 +54,35 @@ const chatUrl = computed(() => {
   url.searchParams.set("dark_theme", "1");
   return url.toString();
 });
+
+function togglePosition() {
+  isRight.value = !isRight.value;
+}
 </script>
 
 <template>
-  <div
-    class="absolute bottom-[-86px] right-[-80px] top-[-90px] w-[300px] overflow-hidden opacity-80"
-  >
-    <iframe class="absolute left-[-55px] size-full" :src="chatUrl" />
+  <div class="absolute top-0" :style="containerStyle">
+    <div class="absolute w-[300px] overflow-hidden opacity-80" :style="clipStyle">
+      <iframe class="absolute size-full" :src="chatUrl" />
+    </div>
+
+    <button
+      class="absolute top-[calc(100%-8px)] grid size-11 place-items-center rounded-full shadow-lg"
+      :class="{
+        'left-0': !isRight,
+        'right-0': isRight,
+      }"
+      @click="togglePosition"
+    >
+      <div class="grid size-6 place-items-center rounded-full bg-black/50">
+        <i
+          class="size-5 text-white/50"
+          :class="{
+            'i-mdi-arrow-right': !isRight,
+            'i-mdi-arrow-left': isRight,
+          }"
+        />
+      </div>
+    </button>
   </div>
 </template>
